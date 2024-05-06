13331248f7266ea8749a996c3e25cbf379f01587
ShreyasGowdaa <sshreyas253@gmail.com>
2024-05-06 08:46:40 +0530

    initial state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hyy i love u so much bbb
         </p>
         <a
           className="App-link"
